5980f79f66b5aad25fb74745f89355fcff4d1ed7
Le Phi Truyen <104491645+leptryn@users.noreply.github.com>
2026-08-04 16:29:53 +0700

    [TV5] Fix item sprites to use items_objects.png with correct frame rects

--- src/items/FireFlower.cpp
+++ src/items/FireFlower.cpp
@@ -10,25 +10,33 @@
 #include "entities/Mario.h"
 #include "patterns/EventBus.h"
 #include "patterns/EventType.h"
+#include "core/AnimationSystem.h"
+#include "core/SpriteFrames.h"
 
 namespace {
 constexpr float FIRE_FLOWER_WIDTH  = 32.f;
 constexpr float FIRE_FLOWER_HEIGHT = 32.f;
 
 constexpr const char* FIRE_FLOWER_TEXTURE_PATH =
-    "assets/textures/items/fireflower.png";
+    "assets/textures/items/items_objects.png";
 } // namespace
 
 FireFlower::FireFlower()
     : Item(sf::Vector2f(0.f, 0.f), sf::Vector2f(FIRE_FLOWER_WIDTH, FIRE_FLOWER_HEIGHT)) {
     initPhysics(nullptr, b2_staticBody, sf::Vector2f(FIRE_FLOWER_WIDTH, FIRE_FLOWER_HEIGHT), true);
     setSprite(FIRE_FLOWER_TEXTURE_PATH);
+    m_animationSystem->addAnimation("idle",
+        AnimationSystem::createManualAnimation(SpriteFrames::Items::fireFlowerFrames(), 0.15f));
+    playAnimation("idle");
 }
 
 FireFlower::FireFlower(const sf::Vector2f& position, b2World* world)
     : Item(position, sf::Vector2f(FIRE_FLOWER_WIDTH, FIRE_FLOWER_HEIGHT)) {
     initPhysics(world, b2_staticBody, sf::Vector2f(FIRE_FLOWER_WIDTH, FIRE_FLOWER_HEIGHT), true);
     setSprite(FIRE_FLOWER_TEXTURE_PATH);
+    m_animationSystem->addAnimation("idle",
+        AnimationSystem::createManualAnimation(SpriteFrames::Items::fireFlowerFrames(), 0.15f));
+    playAnimation("idle");
 }
 
 void FireFlower::update(float dt) {

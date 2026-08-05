8babc303416c9d97ebefa989fdc2219e8f28e088
Le Phi Truyen <104491645+leptryn@users.noreply.github.com>
2026-08-05 17:16:13 +0700

    [TV5] Fix coin popup physics and sprite scaling

--- src/items/Coin.cpp
+++ src/items/Coin.cpp
@@ -14,6 +14,14 @@ namespace {
 constexpr float COIN_WIDTH  = 16.f;
 constexpr float COIN_HEIGHT = 32.f;
 constexpr const char* COIN_TEXTURE_PATH = "assets/textures/items/items_objects.png";
+
+void scaleCoinSprite(sf::Sprite& sprite, const sf::Vector2f& size) {
+    sf::IntRect textureRect = sprite.getTextureRect();
+    if (textureRect.size.x > 0 && textureRect.size.y > 0) {
+        sprite.setScale({size.x / static_cast<float>(textureRect.size.x),
+                         size.y / static_cast<float>(textureRect.size.y)});
+    }
+}
 } // namespace
 
 #include <cmath>
@@ -33,7 +41,9 @@ Coin::Coin(const sf::Vector2f& position, b2World* world, CoinType type)
     : Item(position, sf::Vector2f(COIN_WIDTH, COIN_HEIGHT)),
       m_type(type),
       m_initialY(position.y) {
-    initPhysics(world, b2_staticBody, sf::Vector2f(COIN_WIDTH, COIN_HEIGHT), true);
+    if (m_type == CoinType::COLLECTIBLE) {
+        initPhysics(world, b2_staticBody, sf::Vector2f(COIN_WIDTH, COIN_HEIGHT), true);
+    }
     setSprite(COIN_TEXTURE_PATH);
 
     float frameDuration = (type == CoinType::QUESTION_POPUP) ? 0.08f : 0.2f;
@@ -60,9 +70,15 @@ void Coin::update(float dt) {
             m_sprite->setPosition(m_position);
         }
         updateAnimation(dt);
+        if (m_sprite) {
+            scaleCoinSprite(*m_sprite, m_size);
+        }
     } else {
         syncPhysics();
         updateAnimation(dt);
+        if (m_sprite) {
+            scaleCoinSprite(*m_sprite, m_size);
+        }
     }
 }
 

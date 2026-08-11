f3381764a581d7eb6b1f73401ece04adfe6a1adb
thvy2507 <vyhongtran0205@gmail.com>
2026-08-11 19:16:32 +0700

    [TV1] Complete shell defeat integration

--- tests/SpriteFramesThemeTests.cpp
+++ tests/SpriteFramesThemeTests.cpp
@@ -158,10 +158,10 @@ void testCanonicalEnemyCoordinates() {
 void testCanonicalTileCoordinates() {
     assert(hasPosition(TileFrames::GROUND, 0, 16));
     assert(hasPosition(TileFrames::BRICK, 17, 16));
-    assert(hasPosition(TileFrames::STONE, 34, 16));
+    assert(hasPosition(TileFrames::STONE, 0, 33));
     assert(hasPosition(TileFrames::BRICK_UNDERGROUND, 164, 16));
     assert(hasPosition(TileFrames::BRICK_VARIANT_UNDERGROUND, 181, 16));
-    assert(hasPosition(TileFrames::STONE_UNDERGROUND, 198, 16));
+    assert(hasPosition(TileFrames::STONE_UNDERGROUND, 147, 33));
     assert(hasPosition(TileFrames::HARD_BLOCK_UNDERGROUND, 198, 16));
 
     // Question/used blocks are in Palette 3, not beside the terrain quartet.
